011497721f3890f314b75a856402d666b11e056f
Acer <Acer@DESKTOP-ABKHEEV>
2025-06-03 17:14:38 +0530

    check box, radiobutton, dropdownlist and listbox html helper code added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlHelperOct020625.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -20,6 +21,48 @@ namespace HtmlHelperOct020625.Controllers
         }
 
         public ActionResult Method1()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult CheckBoxDemo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CheckBoxDemo(bool Marathi,Boolean Hindi,bool English)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RadioButtonDemo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RadioButtonDemo(String Gender,String Course)
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DropDownListDemo()
+        {
+            List<PrgLanguages> PrgLangs = new List<PrgLanguages>() { 
+            new PrgLanguages(){ ID=1, Name="JAVA"},
+            new PrgLanguages(){ ID=2,Name="Dot Net"},
+            new PrgLanguages(){ ID=3,Name="Python"},
+            };
+            List<String> Cities = new List<string>()
+            {
+                "Pune","Nashik","Nagar"
+            };
+            ViewBag.cityList = Cities;
+            ViewBag.PrgLangList = PrgLangs;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DropDownListDemo(int course,String OtherCourse,String City)
         {
             return View();
         }
---
+++ Models/PrgLanguages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HtmlHelperOct020625.Models
+{
+    public class PrgLanguages
+    {
+        public int ID { get; set; }
+        public String  Name { get; set; }
+    }
+}
